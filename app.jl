d6ca21324636bd1e27462cb0aadb1d7e27faf3ff
Daniel Friedrich <friedrich@gmx.li>
2021-09-04 15:32:02 +0100

    Add odds ratio testing

--- app.jl
+++ app.jl
@@ -1,5 +1,57 @@
 using CSV, DataFrames, HTTP, PlotlyJS
 using Dash, DashHtmlComponents, DashCoreComponents
+using HypothesisTests
+
+# Function to calculate the statistics
+function calc_statistics(df_tmp, base, base_categories, comp, comp_categories, print_flag=false)
+    total_number = size(df_tmp)[1]
+    # Generate empty arrays for the statistic calculation
+    num_second = zeros(Int64, 2)
+    df_second = []
+
+    # Extract the two categories and calculate the numbers in each
+    for i = 1:2
+        push!(df_second, filter(row -> row[comp] in comp_categories[i], df_tmp))
+        num_second[i] = size(df_second[i])[1]
+    end
+
+    # Generate the matrix for the chi^2 test
+    num_array = zeros(Int64, 2, 2)
+    for i = 1:2
+        for j = 1:2
+            #num_array[i, j] = size(filter(row -> row.TemperatureInsideHome in base.Categories[i], df_second[j]))[1]
+            num_array[i, j] = size(filter(row -> row[base] in base_categories[i], df_second[j]))[1]
+        end
+    end
+
+    # Calculate the Chi^2 value
+    result = ChisqTest(num_array)
+    chi_sq = round(result.stat, digits=3)
+    p_value = pvalue(result)
+    perc_comp1 = round((num_array[1,1] + num_array[2,1])/total_number*100, digits=2)
+    perc_comp1_base1 = round(num_array[1,1]/total_number*100, digits=2)
+    
+    # Odds ratio and 95% confidence interval
+    odds_ratio = round((num_array[1,1]/num_array[2,1]) / (num_array[1,2]/num_array[2,2]), digits=3)
+    upper95 = round(exp(log(odds_ratio) + 1.96 * sqrt(1/num_array[1,1] + 1/num_array[2,1] + 1/num_array[1,2] +1/num_array[2,2])), digits=3)
+    lower95 = round(exp(log(odds_ratio) - 1.96 * sqrt(1/num_array[1,1] + 1/num_array[2,1] + 1/num_array[1,2] +1/num_array[2,2])), digits=3)
+    
+    # Print for checking
+    if print_flag
+        println("Base case: ", base)
+        println("\tCategory 1: ", string(base_categories[1])) 
+        println("\tCategory 2: ", string(base_categories[2]))
+        println(comp, ":")
+        println("\t", num_second[1], " (", perc_comp1, "%) in Category 1: ", string(comp_categories[1]))
+        println("\t", num_second[2], " (", 100 - perc_comp1, "%) in Category 2: ", string(comp_categories[2]))
+        println("Data array: ", num_array)
+        println("Chi^2 = ", chi_sq, ", p-value = ", p_value)
+        println("Odds ratio = ", odds_ratio, ", CI: ", lower95, "-", upper95, "\n")
+    end
+
+    return chi_sq, p_value, odds_ratio, lower95, upper95, perc_comp1, perc_comp1_base1, num_array
+end
+
 
 ## Load COVID-19/Heat data
 filename = "Covid-19_Heat_combined_survey.csv"
@@ -102,6 +154,8 @@ app.layout = html_div() do
         ],
         style = (width = "50%", display = "inline-block", float = "right"),
     ),
+    html_hr(),
+    html_div(id = "stats-values"),
     html_p(
         children = [
             html_hr(),
@@ -200,7 +254,7 @@ callback!(
     Input("xaxis-column", "value"),
 ) do selected_base
     categories = [(label = i, value = i) for i in unique(df_all[!, selected_base])]
-    return categories, categories[1].value, categories, categories[end].value
+    return categories, [categories[1].value], categories, [categories[end].value]
 end
 
 # Update the category dropdown menues for the comparison response
@@ -213,7 +267,62 @@ callback!(
     Input("yaxis-column", "value"),
 ) do selected_comparison
     categories = [(label = i, value = i) for i in unique(df_all[!, selected_comparison])]
-    return categories, categories[1].value, categories, categories[end].value
+    return categories, [categories[1].value], categories, [categories[end].value]
+end
+
+# Calculate and print the statistics values
+callback!(
+    app,
+    Output("stats-values", "children"),
+    Input("xaxis-column", "value"),
+    Input("base_cat1", "value"), 
+    Input("base_cat2", "value"), 
+    Input("yaxis-column", "value"),
+    Input("comparison_cat1", "value"), 
+    Input("comparison_cat2", "value"), 
+    Input("country_selector", "value"),
+) do base, base_cat1, base_cat2, comp, comp_cat1, comp_cat2, countries
+    cat1 = Vector{String}()
+    for i in comp_cat1
+        push!(cat1, i)
+    end
+
+    cat2 = Vector{String}()
+    for i in comp_cat2
+        push!(cat2, i)
+    end
+
+    comp_categories = [cat1, cat2]
+
+    cat1 = Vector{String}()
+    for i in base_cat1
+        push!(cat1, i)
+    end
+
+    cat2 = Vector{String}()
+    for i in base_cat2
+        push!(cat2, i)
+    end
+
+    base_categories = [cat1, cat2]
+
+    # Filter the selected countries
+    df_CH = filter(row -> row.Country in countries, df_all);
+
+    chi_sq, p_value, odds_ratio, lower95, upper95, perc_comp1, perc_comp1_base1, num_array = calc_statistics(df_CH, base, base_categories, comp, comp_categories, false)
+
+    significant = "not statistically significant"
+    # Check significance for lowre odds ratio
+    if (lower95 < 1.0 && upper95 < 1.0) 
+        significant = "statistically significant and the odds ratio is lower"
+    end
+    # Check significance for higher odds ratio
+    if (lower95 > 1.0 && upper95 > 1.0)
+        significant = "statistically significant and the odds ratio is higher"
+    end    
+
+
+    return "The odds to report $(base) of $(base_cat1) is $(odds_ratio) for respondents in $(comp) - $(comp_cat1) compared to $(comp) - $(comp_cat2). The result is $(significant)."
 end
 
 run_server(app, "0.0.0.0", debug = true)
